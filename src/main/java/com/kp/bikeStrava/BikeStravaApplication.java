a18ec9e3e528c67fef0c06c131bdff1abeb096f8
Krzysiek <krzysiek.pienkowski@gmail.com>
2024-01-26 22:11:56 +0100

    Initial commit

---
+++ src/main/java/com/kp/bikeStrava/BikeStravaApplication.java
@@ -0,0 +1,13 @@
+package com.kp.bikeStrava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BikeStravaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BikeStravaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kp/bikeStrava/BikeStravaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kp.bikeStrava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BikeStravaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
